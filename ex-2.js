04272d9d73102960ad6a2277f38adb8fec1ea545
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-02-24 14:32:25 +0700

    fix: misunderstood assignment question

--- ex-2.js
+++ ex-2.js
@@ -1,11 +1,18 @@
 // Exercise #2: Promotion Conditions
+const john = {
+    amtPaidLastMonth: 4001,
+    boughDate: 'Friday',
+    hasBoughtProductFromITCategory: true,
+    hasAttendedDiscountEvent: true,
+    memberRank: 'Gold'
+}
 
 // Start coding here
-let lastMonthPaidMoreThan4000 = true;
-let isWeekday = true;
-let hasBoughtProductFromITCategory = false;
-let hasAttendedDiscountEvent = true;
-let isPlatinum = false;
+let lastMonthPaidMoreThan4000 = john.amtPaidLastMonth > 4000;
+let isWeekday = john.boughDate !== 'Saturday' && john.boughDate !== 'Sunday';
+let hasBoughtProductFromITCategory = john.hasBoughtProductFromITCategory;
+let hasAttendedDiscountEvent = john.hasAttendedDiscountEvent;
+let isPlatinum = 'Platinum' === john.memberRank;
 
 let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday &&
     !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent)
